26231990326f9df9fbc72be154e05f7ecf2582bc
Leila Skinner <beileiwang@gmail.com>
2020-08-28 17:51:10 -0500

    if two cards are the same, they disappear

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -8,243 +8,254 @@ type CardsFlippedState = {
 
 function Cards() {
   const [lastClicked, setLastClicked] = useState<CardsFlippedState>();
-  const [cards, setCards] = useState([]);
-
-  useEffect(() => {});
+  const [gameOver, setGameOver] = useState(false);
 
   const cardsDivs = document.querySelectorAll(".memory-card");
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    console.log(event.currentTarget);
-    console.log(event.currentTarget.dataset);
-    event.currentTarget.classList.add("flip");
-    console.log(lastClicked);
+    const currentClicked = event.currentTarget;
+    currentClicked.classList.add("flip");
     if (!lastClicked) {
       setLastClicked({
-        cardId: event.currentTarget.dataset.id || "",
-        name: event.currentTarget.dataset.name || "",
+        cardId: currentClicked.dataset.id || "",
+        name: currentClicked.dataset.name || "",
       });
     } else {
-      if (lastClicked.name !== event.currentTarget.dataset.name) {
-        setLastClicked(undefined);
+      setLastClicked(undefined);
+      if (lastClicked.name === currentClicked.dataset.name) {
+        setTimeout(() => {
+          cardsDivs.forEach((card) => {
+            if (
+              card.getAttribute("data-name") === currentClicked.dataset.name
+            ) {
+              card.classList.add("image-opacity");
+            }
+          });
+        }, 1000);
+      } else {
         setTimeout(() => {
           cardsDivs.forEach((card) => {
             card.classList.remove("flip");
           });
         }, 1000);
-
-        console.log(cardsDivs);
       }
     }
   };
 
   return (
-    <div className="card-container">
-      <div
-        className="memory-card"
-        data-name="daniel"
-        data-id="1"
-        onClick={handleClick}
-      >
-        <img
-          className="back"
-          src={require("../../images/joel.jpg")}
-          alt="Card Back"
-        />
-        <img
-          className="front"
-          src={require("../../images/daniel.jpg")}
-          alt="Card Front"
-        />
+    <>
+      {gameOver === true && (
+        <button onClick={() => console.log("Start Again Clicked")}>
+          Start Again
+        </button>
+      )}
+      <div className="card-container">
+        <div
+          className="memory-card"
+          data-name="daniel"
+          data-id="1"
+          onClick={handleClick}
+        >
+          <img
+            className="back"
+            src={require("../../images/joel.jpg")}
+            alt="Card Back"
+          />
+          <img
+            className="front"
+            src={require("../../images/daniel.jpg")}
+            alt="Card Front"
+          />
+        </div>
+        <div
+          className="memory-card"
+          data-name="daniel"
+          data-id="2"
+          onClick={handleClick}
+        >
+          <img
+            className="back"
+            src={require("../../images/joel.jpg")}
+            alt="Card Back"
+          />
+          <img
+            className="front"
+            src={require("../../images/daniel.jpg")}
+            alt="Card Front"
+          />
+        </div>
+        <div
+          className="memory-card"
+          data-name="david"
+          data-id="3"
+          onClick={handleClick}
+        >
+          <img
+            className="back"
+            src={require("../../images/joel.jpg")}
+            alt="Card Back"
+          />
+          <img
+            className="front"
+            src={require("../../images/david.jpg")}
+            alt="Card Front"
+          />
+        </div>
+        <div
+          className="memory-card"
+          data-name="david"
+          data-id="4"
+          onClick={handleClick}
+        >
+          <img
+            className="back"
+            src={require("../../images/joel.jpg")}
+            alt="Card Back"
+          />
+          <img
+            className="front"
+            src={require("../../images/david.jpg")}
+            alt="Card Front"
+          />
+        </div>
+        <div
+          className="memory-card"
+          data-name="edgar"
+          data-id="5"
+          onClick={handleClick}
+        >
+          <img
+            className="back"
+            src={require("../../images/joel.jpg")}
+            alt="Card Back"
+          />
+          <img
+            className="front"
+            src={require("../../images/edgar.jpg")}
+            alt="Card Front"
+          />
+        </div>
+        <div
+          className="memory-card"
+          data-name="edgar"
+          data-id="6"
+          onClick={handleClick}
+        >
+          <img
+            className="back"
+            src={require("../../images/joel.jpg")}
+            alt="Card Back"
+          />
+          <img
+            className="front"
+            src={require("../../images/edgar.jpg")}
+            alt="Card Front"
+          />
+        </div>
+        <div
+          className="memory-card"
+          data-name="ian"
+          data-id="7"
+          onClick={handleClick}
+        >
+          <img
+            className="back"
+            src={require("../../images/joel.jpg")}
+            alt="Card Back"
+          />
+          <img
+            className="front"
+            src={require("../../images/ian.jpg")}
+            alt="Card Front"
+          />
+        </div>
+        <div
+          className="memory-card"
+          data-name="ian"
+          data-id="8"
+          onClick={handleClick}
+        >
+          <img
+            className="back"
+            src={require("../../images/joel.jpg")}
+            alt="Card Back"
+          />
+          <img
+            className="front"
+            src={require("../../images/ian.jpg")}
+            alt="Card Front"
+          />
+        </div>
+        <div
+          className="memory-card"
+          data-name="linnea"
+          data-id="9"
+          onClick={handleClick}
+        >
+          <img
+            className="back"
+            src={require("../../images/joel.jpg")}
+            alt="Card Back"
+          />
+          <img
+            className="front"
+            src={require("../../images/linnea.jpg")}
+            alt="Card Front"
+          />
+        </div>
+        <div
+          className="memory-card"
+          data-name="linnea"
+          data-id="10"
+          onClick={handleClick}
+        >
+          <img
+            className="back"
+            src={require("../../images/joel.jpg")}
+            alt="Card Back"
+          />
+          <img
+            className="front"
+            src={require("../../images/linnea.jpg")}
+            alt="Card Front"
+          />
+        </div>
+        <div
+          className="memory-card"
+          data-name="ray"
+          data-id="11"
+          onClick={handleClick}
+        >
+          <img
+            className="back"
+            src={require("../../images/joel.jpg")}
+            alt="Card Back"
+          />
+          <img
+            className="front"
+            src={require("../../images/ray.jpg")}
+            alt="Card Front"
+          />
+        </div>
+        <div
+          className="memory-card"
+          data-name="ray"
+          data-id="12"
+          onClick={handleClick}
+        >
+          <img
+            className="back"
+            src={require("../../images/joel.jpg")}
+            alt="Card Back"
+          />
+          <img
+            className="front"
+            src={require("../../images/ray.jpg")}
+            alt="Card Front"
+          />
+        </div>
       </div>
-      <div
-        className="memory-card"
-        data-name="daniel"
-        data-id="2"
-        onClick={handleClick}
-      >
-        <img
-          className="back"
-          src={require("../../images/joel.jpg")}
-          alt="Card Back"
-        />
-        <img
-          className="front"
-          src={require("../../images/daniel.jpg")}
-          alt="Card Front"
-        />
-      </div>
-      <div
-        className="memory-card"
-        data-name="david"
-        data-id="3"
-        onClick={handleClick}
-      >
-        <img
-          className="back"
-          src={require("../../images/joel.jpg")}
-          alt="Card Back"
-        />
-        <img
-          className="front"
-          src={require("../../images/david.jpg")}
-          alt="Card Front"
-        />
-      </div>
-      <div
-        className="memory-card"
-        data-name="david"
-        data-id="4"
-        onClick={handleClick}
-      >
-        <img
-          className="back"
-          src={require("../../images/joel.jpg")}
-          alt="Card Back"
-        />
-        <img
-          className="front"
-          src={require("../../images/david.jpg")}
-          alt="Card Front"
-        />
-      </div>
-      <div
-        className="memory-card"
-        data-name="edgar"
-        data-id="5"
-        onClick={handleClick}
-      >
-        <img
-          className="back"
-          src={require("../../images/joel.jpg")}
-          alt="Card Back"
-        />
-        <img
-          className="front"
-          src={require("../../images/edgar.jpg")}
-          alt="Card Front"
-        />
-      </div>
-      <div
-        className="memory-card"
-        data-name="edgar"
-        data-id="6"
-        onClick={handleClick}
-      >
-        <img
-          className="back"
-          src={require("../../images/joel.jpg")}
-          alt="Card Back"
-        />
-        <img
-          className="front"
-          src={require("../../images/edgar.jpg")}
-          alt="Card Front"
-        />
-      </div>
-      <div
-        className="memory-card"
-        data-name="ian"
-        data-id="7"
-        onClick={handleClick}
-      >
-        <img
-          className="back"
-          src={require("../../images/joel.jpg")}
-          alt="Card Back"
-        />
-        <img
-          className="front"
-          src={require("../../images/ian.jpg")}
-          alt="Card Front"
-        />
-      </div>
-      <div
-        className="memory-card"
-        data-name="ian"
-        data-id="8"
-        onClick={handleClick}
-      >
-        <img
-          className="back"
-          src={require("../../images/joel.jpg")}
-          alt="Card Back"
-        />
-        <img
-          className="front"
-          src={require("../../images/ian.jpg")}
-          alt="Card Front"
-        />
-      </div>
-      <div
-        className="memory-card"
-        data-name="linnea"
-        data-id="9"
-        onClick={handleClick}
-      >
-        <img
-          className="back"
-          src={require("../../images/joel.jpg")}
-          alt="Card Back"
-        />
-        <img
-          className="front"
-          src={require("../../images/linnea.jpg")}
-          alt="Card Front"
-        />
-      </div>
-      <div
-        className="memory-card"
-        data-name="linnea"
-        data-id="10"
-        onClick={handleClick}
-      >
-        <img
-          className="back"
-          src={require("../../images/joel.jpg")}
-          alt="Card Back"
-        />
-        <img
-          className="front"
-          src={require("../../images/linnea.jpg")}
-          alt="Card Front"
-        />
-      </div>
-      <div
-        className="memory-card"
-        data-name="ray"
-        data-id="11"
-        onClick={handleClick}
-      >
-        <img
-          className="back"
-          src={require("../../images/joel.jpg")}
-          alt="Card Back"
-        />
-        <img
-          className="front"
-          src={require("../../images/ray.jpg")}
-          alt="Card Front"
-        />
-      </div>
-      <div
-        className="memory-card"
-        data-name="ray"
-        data-id="12"
-        onClick={handleClick}
-      >
-        <img
-          className="back"
-          src={require("../../images/joel.jpg")}
-          alt="Card Back"
-        />
-        <img
-          className="front"
-          src={require("../../images/ray.jpg")}
-          alt="Card Front"
-        />
-      </div>
-    </div>
+    </>
   );
 }
 
