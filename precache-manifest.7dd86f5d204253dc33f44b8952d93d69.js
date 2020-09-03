a7386cc00850b57864c6e1d9abc883fad147a57d
Leila Skinner <beileiwang@gmail.com>
2020-09-03 10:41:51 -0500

    Updates

---
+++ precache-manifest.7dd86f5d204253dc33f44b8952d93d69.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8202b2c757375d46ddcdbaaf4eb69842",
+    "url": "/react-typescript/index.html"
+  },
+  {
+    "revision": "6c24afa691d853249507",
+    "url": "/react-typescript/static/css/main.904867d6.chunk.css"
+  },
+  {
+    "revision": "83f26a41d2536e46e0bb",
+    "url": "/react-typescript/static/js/2.93a9e59d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-typescript/static/js/2.93a9e59d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c24afa691d853249507",
+    "url": "/react-typescript/static/js/main.e9877966.chunk.js"
+  },
+  {
+    "revision": "5121af9c068a16e705f9",
+    "url": "/react-typescript/static/js/runtime-main.f51b0758.js"
+  },
+  {
+    "revision": "1e4d7ad334472d1cf44c4381ce02325d",
+    "url": "/react-typescript/static/media/daniel.1e4d7ad3.jpg"
+  },
+  {
+    "revision": "1eadd4b70835e85c183a4779f0b589e8",
+    "url": "/react-typescript/static/media/david.1eadd4b7.jpg"
+  },
+  {
+    "revision": "21923eb5adba65d048f013e3ae0c41d7",
+    "url": "/react-typescript/static/media/edgar.21923eb5.jpg"
+  },
+  {
+    "revision": "527f64f3432838e7bb0de0d2288a70a6",
+    "url": "/react-typescript/static/media/ian.527f64f3.jpg"
+  },
+  {
+    "revision": "6dd4c14e214fa24fdfd0136c2ff96cd6",
+    "url": "/react-typescript/static/media/joel.6dd4c14e.jpg"
+  },
+  {
+    "revision": "fbd774392ba05f1bcca4cfd1a2b029a5",
+    "url": "/react-typescript/static/media/linnea.fbd77439.jpg"
+  },
+  {
+    "revision": "3d17f0b31a46aae7eae506de0a3f0bf5",
+    "url": "/react-typescript/static/media/ray.3d17f0b3.jpg"
+  }
+]);
